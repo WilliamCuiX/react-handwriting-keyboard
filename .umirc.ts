c22dacc0d56ad4979a80194bfc1d2736dcd640c1
William Cui <444738577@qq.com>
2020-09-07 17:49:49 +0800

    增加dumi开发依赖

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,5 @@ export default {
   mode: 'doc',
   title: 'react-handwriting-keyboard',
   base: '/react-handwriting-keyboard',
-  publicPath: '/react-handwriting-keyboard/',
-  alias: {
-    'react-handwriting-keyboard': './'
-  }
+  publicPath: '/react-handwriting-keyboard/'
 };
